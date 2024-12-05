04b804125bb781ba365f87d1206c9bddf19800c2
unbreak4ble <diego.gamere1921@outlook.com>
2024-12-04 21:04:17 -0400

    created toggle-switch component

--- frontend/src/app/shared/components/toggle-switch/toggle-switch.component.ts
+++ frontend/src/app/shared/components/toggle-switch/toggle-switch.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-toggle-switch',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './toggle-switch.component.scss'
 })
 export class ToggleSwitchComponent {
+  readonly uuid:string = 'id_'+uuid.v4();
+  @Input('text') text:string = '';
+  _document:Element|null = null;
+  checked:boolean = false;
 
+  constructor(){
+
+  }
+
+  ngAfterViewInit(){
+    this._document = document.getElementById(this.uuid);
+  }
+
+  toggle(){
+    const element = this._document?.querySelector('input');
+
+    if(element == null) return;
+
+    element.checked = this.checked = !element.checked;
+  }
 }
