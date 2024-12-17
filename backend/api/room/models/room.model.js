7e157681ce5ae3332df44f5b943c8afca79522ea
unbreak4ble <diego.gamere1921@outlook.com>
2024-12-16 20:12:19 -0400

    Reforced payload validation

--- backend/api/room/models/room.model.js
+++ backend/api/room/models/room.model.js
@@ -1,10 +1,13 @@
 const Joi = require('joi');
 const { questionSchema } = require("./question.model");
+const { settingSchema } = require('./setting.model');
+
+const reg_name = /^[a-zA-Z]+$/;
 
 const roomSchema = Joi.object({
-    text: Joi.string().max(64).min(2),
+    text: Joi.string().max(32).min(2).regex(reg_name).required(),
     questions: Joi.array().items(questionSchema).min(1).max(256),
-    settings: Joi.array()
+    settings: Joi.array().items(settingSchema).max(16)
 });
 
 module.exports = {
